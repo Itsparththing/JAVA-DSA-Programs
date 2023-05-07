95f82da0d8f2e5fe7a4c0c1dce2e032f1d399569
Parth Agrawal <86774143+Itsparththing@users.noreply.github.com>
2023-05-07 21:22:53 +0530

    Create Arrays: Q24: Leetcode-860 Lemonade Change.java

---
+++ Arrays: Q24: Leetcode-860 Lemonade Change.java
@@ -0,0 +1,23 @@
+
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0;
+        int ten = 0;
+        for (int i : bills) {
+            if (i == 5) 
+                five++;
+            else if (i == 10) {
+                five--; ten++;
+            }
+            else if (ten > 0 && five>=1) { 
+                ten--; five--;
+            }
+            else     
+                five -= 3;
+
+            if (five < 0)  
+                return false;
+        }
+        return true;
+    }
+}
