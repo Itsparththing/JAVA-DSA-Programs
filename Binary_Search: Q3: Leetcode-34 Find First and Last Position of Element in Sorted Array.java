8587a534b7f0a35989a9aa80b104ef39e3b1d83c
Parth Agrawal <86774143+Itsparththing@users.noreply.github.com>
2023-05-17 23:26:23 +0530

    Update Binary_Search: Q3: Leetcode-34 Find First and Last Position of Element in Sorted Array.java

--- Binary_Search: Q3: Leetcode-34 Find First and Last Position of Element in Sorted Array.java
+++ Binary_Search: Q3: Leetcode-34 Find First and Last Position of Element in Sorted Array.java
@@ -1,3 +1,6 @@
+// My solution 
+// Time Complexity:- O(n)
+
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         int[] result = new int[2];
@@ -19,3 +22,55 @@ class Solution {
         return result;
     }
 }
+
+
+// Optimised Solution
+// Time Complexity:- O(Log(n)) 
+
+import java.util.Arrays;
+
+public class firstandlastindexoftarge {
+    public static void main(String[] args) {
+        int nums[]= {1,4,5,5,5,6,7,9,12};
+        int target = 5;
+        int []ans ={-1,-1};
+        int start =search(nums, target, true);
+        int end =search(nums, target, false);
+        ans[0]=start;
+        ans[1]=end;
+
+        System.out.println(Arrays.toString(ans));
+    }
+    
+    static int search(int[] arr, int target, boolean finStartIndex){
+        int ans = -1;
+        int start =0;
+        int end = arr.length -1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+
+            if(target>arr[arr.length-1]){
+                return -1;
+            }
+            if(target<arr[mid]){
+                end = mid-1;
+
+            } else if (target>arr[mid]) {
+                start = mid+1;
+            }
+            else{
+                ans = mid;
+                if(finStartIndex==true){
+                    end = mid-1;
+                }
+                else {
+                    start = mid+1;
+                }
+
+            }
+        }
+        return ans;
+    }
+}
+
+
