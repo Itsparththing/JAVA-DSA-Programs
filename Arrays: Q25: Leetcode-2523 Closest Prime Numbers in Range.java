9b16625bf398a15c323a9222fa90fdbb13685bb9
Parth Agrawal <86774143+Itsparththing@users.noreply.github.com>
2023-06-16 11:44:08 +0530

    Update Arrays: Q25: Leetcode-2523 Closest Prime Numbers in Range.java

--- Arrays: Q25: Leetcode-2523 Closest Prime Numbers in Range.java
+++ Arrays: Q25: Leetcode-2523 Closest Prime Numbers in Range.java
@@ -1,3 +1,48 @@
+//Optimal solution
+
+class Solution {
+    public int[] closestPrimes(int left, int right) {
+        int[] nums = {-1, -1};
+        int min = Integer.MAX_VALUE;
+        List<Integer> list = new ArrayList<>();
+        for (int i = left; i <= right; i++){
+            if (isPrime(i)){
+                list.add(i);
+            }
+        }
+        if (list.isEmpty() || list.size() < 2){
+            return nums;
+        }
+        for (int i = 1; i < list.size(); i++){
+            int temp = list.get(i) - list.get(i-1);
+            if (min > temp){
+                min = temp;
+                nums[0] = list.get(i-1);
+                nums[1] = list.get(i);
+            }
+        }
+        return nums;
+    }
+
+    boolean isPrime(int n)
+    {
+        if (n <= 1)
+            return false;
+        if (n <= 3)
+            return true;
+        if (n % 2 == 0 || n % 3 == 0)
+            return false;
+        for (int i = 5; i * i <= n; i = i + 6)
+            if (n % i == 0 || n % (i + 2) == 0)
+                return false;
+        return true;
+    }
+}
+
+
+
+//not so cool solution
+
 class Solution {
     public int[] closestPrimes(int left, int right) {
         int pnum[] =  {-1,-1};
@@ -35,3 +80,4 @@ class Solution {
         return true;
     }
 }
+
